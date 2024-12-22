54cd7d864fec738e2961aed01438237684b72350
ozz1webdev <ozz1webdev@gmail.com>
2024-12-22 21:50:20 +0100

    creating hamburger overlay menu

--- project/project/settings.py
+++ project/project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l#%-v*7aus)z&93$#77jjx9($4z5vuvha#smej$^#w&u^wyzwb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
