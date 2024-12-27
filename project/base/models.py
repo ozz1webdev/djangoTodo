a675aef210d6bfd93c6b72d675b1cf5ac4109484
ozz1webdev <ozz1webdev@gmail.com>
2024-12-27 23:21:54 +0100

    adding model and views

--- project/base/models.py
+++ project/base/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+state = (
+    ('ToDo', 'To Do'),
+    ('In Progress', 'In Progress'),
+    ('Done', 'Done'),
+)
+
+
+class Task(models.Model):
+    owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    project = models.CharField(max_length=255, null=False, blank=False)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    state = models.CharField(max_length=255, default='ToDo', choices=state)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created_at']
--- project/base/views.py
+++ project/base/views.py
@@ -7,3 +7,20 @@ def index(request):
 
 def home(request):
     return render(request, 'base/base.html')
+
+
+def addTodo(request):
+    return render(request, 'base/add.html')
+
+
+def viewTodos(request):
+    return render(request, 'base/viewall.html')
+
+
+def deleteTodo(request):
+    return render(request, 'base/delete.html')
+
+
+def viewTodo(request):
+    return render(request, 'base/viewtodo.html')
+
